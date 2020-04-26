ce835919adcfd89e8c04602ef9709bad5afbb785
Matthew Neal <matthewneal1202@gmail.com>
2020-04-26 09:54:32 -0700

    Updates

---
+++ precache-manifest.c2b5387874c8235ed7e0ba605d50bfb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d92ccc4aaed2637800df538517a5e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8fa7d70778ad1bcbca4",
+    "url": "/static/css/main.82d32060.chunk.css"
+  },
+  {
+    "revision": "d9db4591540d4d1b2f55",
+    "url": "/static/js/2.a1dacbcc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a1dacbcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8fa7d70778ad1bcbca4",
+    "url": "/static/js/main.fb0293c5.chunk.js"
+  },
+  {
+    "revision": "9cfe963f8c85cb9d4d23",
+    "url": "/static/js/runtime-main.4ac76773.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hacker-news-react/precache-manifest.9dcb6cac10d1cee375ce8064b2b32c5c.js"
+  "/precache-manifest.c2b5387874c8235ed7e0ba605d50bfb8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hacker-news-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

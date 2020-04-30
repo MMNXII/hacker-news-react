5d5d2b3d2081164ab04908449303b490eadd0517
Matthew Neal <matthewneal1202@gmail.com>
2020-04-30 15:40:44 -0700

    added functionality, with testing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { div, Fragment, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [state, setState] = useState('yes');
+  const toggle = () => {
+    state === 'yes' ? setState('no') : setState('yes');
+  };
+
+  const greeting = 'hello';
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          change to <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Search />
+      <hr />
+
+      <List stories={stories} />
+      <p>{state}</p>
+      <Greeting greeting={greeting} state={state} />
+      <Button onClick={toggle} />
     </div>
   );
-}
+};
+
+const Greeting = ({ greeting, state }) =>
+  state === 'yes' ? <h1>{greeting}</h1> : null;
+
+const Button = ({ onClick }) => (
+  <button onClick={onClick} type='button'>
+    Click
+  </button>
+);
+
+const List = ({ stories }) =>
+  stories.map((item) => (
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </div>
+  ));
+
+const Search = () => {
+  return (
+    <Fragment>
+      <h1>My Hacker Stories</h1>
+
+      <label htmlFor='search'>Search:</label>
+      <input id='search' type='text' maxLength='10' onChange={handleChange} />
+    </Fragment>
+  );
+};
+
+const handleChange = (event) => {
+  const value = event.target.value;
+  console.log(value);
+};
 
 export default App;
